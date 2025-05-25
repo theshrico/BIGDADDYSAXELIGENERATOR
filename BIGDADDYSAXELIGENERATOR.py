573683d8e8823355d1cdc11c459cb8bfd84a80ef
nikabregadze <nickbregadze@gmail.com>
2025-05-25 14:42:55 +0400

    Update BIGDADDYSAXELIGENERATOR.py

--- BIGDADDYSAXELIGENERATOR.py
+++ BIGDADDYSAXELIGENERATOR.py
@@ -4,7 +4,7 @@ print("               welcome to               ")
 print("========================================")
 print("       BIG DADDY SAXELI GENERATOR")
 key = input("sex: type 'm' for male 'f' for female:")
-def bicepshi_(names, surnames):
+def randomtable(names, surnames):
             input("press enter to run:")
             
             num = random.randrange(0,len(names))
@@ -13,10 +13,10 @@ def bicepshi_(names, surnames):
             print(names[num]+ " " + surnames[num2])
 if key == "m":
      while True:
-        bicepshi_(["vaso","imeda","zaura","vasoia","merab","grigola","avto","gocha","givi","shota"],  ["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","doundobelishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"])
+        randomtable(["vaso","imeda","zaura","vasoia","merab","grigola","avto","gocha","givi","shota"],  ["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","doundobelishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"])
 elif key == "f":
     while True:      
-        bicepshi_(["lamara","nargiza","leila","nunu","natela","nedejda","nazibrola"],["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","mchadimcxobiashvili","kuxnashivarmeishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"])
+        randomtable(["lamara","nargiza","leila","nunu","natela","nedejda","nazibrola"],["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","mchadimcxobiashvili","kuxnashivarmeishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"])
 else:
     print("that is not a correct response, exiting...")
     time.sleep(5)
