e0e71a31ee55a921ddb8f4cc3c633189dfeef6c6
nikabregadze <nickbregadze@gmail.com>
2025-05-25 14:41:45 +0400

    Update BIGDADDYSAXELIGENERATOR.py

--- BIGDADDYSAXELIGENERATOR.py
+++ BIGDADDYSAXELIGENERATOR.py
@@ -4,22 +4,19 @@ print("               welcome to               ")
 print("========================================")
 print("       BIG DADDY SAXELI GENERATOR")
 key = input("sex: type 'm' for male 'f' for female:")
+def bicepshi_(names, surnames):
+            input("press enter to run:")
+            
+            num = random.randrange(0,len(names))
+            num2 = random.randrange(0,len(surnames))
+        
+            print(names[num]+ " " + surnames[num2])
 if key == "m":
-    while True:
-        input("press enter to run:")
-        saxelebi = ["vaso","imeda","zaura","vasoia","merab","grigola","avto","gocha","givi","shota"]
-        num = random.randrange(0,len(saxelebi))
-        gvarebi = ["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","doundobelishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"]
-        num2 = random.randrange(0,len(gvarebi))
-        print(saxelebi[num] + " " + gvarebi[num2])
+     while True:
+        bicepshi_(["vaso","imeda","zaura","vasoia","merab","grigola","avto","gocha","givi","shota"],  ["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","doundobelishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"])
 elif key == "f":
-    while True:
-        input("press enter to run:")
-        saxelebi = ["lamara","nargiza","leila","nunu","natela","nedejda","nazibrola"]
-        num = random.randrange(0,len(saxelebi))
-        gvarebi = ["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","mchadimcxobiashvili","kuxnashivarmeishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"]
-        num2 = random.randrange(0,len(gvarebi))
-        print(saxelebi[num] + " " + gvarebi[num2])
+    while True:      
+        bicepshi_(["lamara","nargiza","leila","nunu","natela","nedejda","nazibrola"],["walaxashvili","imerlishvili","avtandiluzbekishvili","ghomimcxobiashvili","mchadimcxobiashvili","kuxnashivarmeishvili","laphshamiashvili","dzroxasadaishvili","virismoyvarulishvili"])
 else:
     print("that is not a correct response, exiting...")
     time.sleep(5)
